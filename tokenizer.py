457fd410108293882b1a7ddf80f0c7d72768e34d
Conspiciens <fernandezjohann08@gmail.com>
2026-05-09 12:35:31 -0700

    Vocab is now dict[int, bytes] and focus now added parameters to class

--- tokenizer.py
+++ tokenizer.py
@@ -13,21 +13,25 @@ class multiTokenizer:
         pass
 
 class Tokenizer: 
-    def __init__(self, input_path: str): 
+    def __init__(self, input_path: str, vocab_size: int, special_tokens: list[str]): 
         self.input_path = input_path
+        self.vocab_size = vocab_size
 
+        # self.vocab: dict[int, bytes]
         self.vocab = defaultdict(int) 
-        self.vocab[b"<|endoftext|>"] = 0 
+        self.vocab[0] = b"<|endoftext|>" 
         self.vocab_count = 1 
 
         for i in range(97,123): 
             letter = chr(i)
 
-            self.vocab[letter.encode()] = self.vocab_count
+            self.vocab[self.vocab_count] = letter.encode()
+            # self.vocab[letter.encode()] = self.vocab_count
             self.vocab_count += 1 
 
         self.merges: list[tuple[bytes, ...]] = []
-        self.special_tokens: list[str] = ["<|endoftext|>", "<|lost|>"]
+        # self.special_tokens: list[str] = ["<|endoftext|>", "<|lost|>"]
+        self.special_tokens: list[str] = special_tokens
 
         self.special_tokens = sorted(self.special_tokens, key=len, reverse=True)
 
@@ -72,7 +76,8 @@ class Tokenizer:
                 word = word.encode() 
                 print(f"Word Type: {type(word)}")
 
-                self.vocab[word] = self.vocab_count
+                # self.vocab[word] = self.vocab_count
+                self.vocab[self.vocab_count] = word
                 self.vocab_count += 1
 
         print("Pre-Tokenization")
@@ -80,10 +85,10 @@ class Tokenizer:
 
         # Tokenization - Begin BPE process
         self.pairs = defaultdict(int) 
-        for i in range(6):
+        while len(self.vocab) < self.vocab_size:
 
-            for (vocab, count) in self.vocab.items(): 
-                if vocab == b"<|endoftext|>" or vocab == "" or len(vocab) <= 1:
+            for (count, vocab) in self.vocab.items(): 
+                if vocab == b"<|endoftext|>" or vocab == b"" or len(vocab) < 1:
                     continue
 
                 print(f"Vocab: {vocab}")
@@ -112,25 +117,29 @@ class Tokenizer:
             print(f"old key type: {type(old_key)}")
 
             new_words = {}
-            for (vocab, count) in self.vocab.items():   
+            for (count, vocab) in self.vocab.items():   
                 print(f"Old Key: {old_key}")
                 print(f"vocab bytes: {vocab}")
+                print(f"count: {count}")
 
                 if old_key in vocab:      
-                    old_word = vocab
                     new_word = vocab.replace(old_key, new_key)
                     print(f"replaced:  {new_word}")
-                    new_words[old_word] = new_word 
+                    new_words[count] = new_word 
 
             self.vocab_count += 1
-            self.vocab[new_key] = self.vocab_count
+            self.vocab[self.vocab_count] = new_key
+            # self.vocab[new_key] = self.vocab_count
+
 
             print("New Words")
             print(new_words)
 
-            for (old_word, new_word) in new_words.items():
-                self.vocab[new_word] = self.vocab[old_word]
-                del self.vocab[old_word]
+            for (count, new_word) in new_words.items():
+                print(f"Old Vocab count: {count}")
+                print(f"New Vocab count: {new_word}")
+                self.vocab[count] = new_word 
+                # self.vocab[new_word] = self.vocab[old_word]
 
             self.pairs.clear() 
 
@@ -146,3 +155,15 @@ class Tokenizer:
 
     def decode(): 
         pass 
+
+
+if __name__ == '__main__': 
+    bpe = Tokenizer("test", 10, ["<|endoftext|>"]) 
+    encodings = bpe.encode('''u don't have to be scared of the loud dog, I'll protect you". The mole felt so safe with the little girl. She was very kind and the mole soon came to trust her. He leaned against her and she kept him safe. The mole had found his best friend.
+<|endoftext|>
+Once upon a time, in a warm and sunny place, there was a big pit. A little boy named Tom liked to play near the pit. One day, Tom lost his red ball. He was very sad.
+Tom asked his friend, Sam, to help him search for the ball. They looked high and low, but they could not find the ball. Tom said, "I think my ball fell into the pit."
+Sam and Tom went close to the pit. They were scared, but they wanted to find the red ball. They looked into the pit, but it was too dark to see. Tom said, "We must go in and search for my ball."
+They went into the pit to search. It was dark and scary. They could not find the ball. They tried to get out, but the pit was too deep. Tom and Sam were stuck in the pit. They called for help, but no one could hear them. They were sad and scared, and they never got out of the pit.
+<|endoftext|>''')
+    print(f"Encodings: {encodings}")
