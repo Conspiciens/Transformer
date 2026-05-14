ab1cfb86643c7a73ce479392f6cadac3bf0fb11a
Conspiciens <fernandezjohann08@gmail.com>
2026-05-13 22:21:11 -0700

    Major fixes since there remain issues when tokenizing, alot of revamping, improved performance, finally works but requires more testing

--- tokenizer.py
+++ tokenizer.py
@@ -1,5 +1,6 @@
 from collections import defaultdict, Counter
 from functools import reduce
+import pathlib
 from pretokenization_example import find_chunk_boundaries
 from multiprocessing import Pool
 
@@ -162,50 +163,55 @@ class Tokenizer:
 
 def pretokenization(text: str, special_token: bytes) -> dict[bytes, int]:
     vocab = {} 
-    vocab_count = 0
+
     paragraphs: list[str] = [] 
 
-    assert isinstance(special_token, bytes)
     # special_token_re = [re.escape(token) for token in special_tokens] 
-    special_token = special_token.decode("utf-8")
-    special_token_re = re.escape(special_token)
+    # special_token = special_token.encode("utf-8")
+    # special_token_re = re.escape(special_token)
     # special_token_re = "|".join(special_token_re.decode("utf-8"))
-    special_token_re = fr"({special_token_re})".encode("utf-8")
-    print(special_token_re)
+    # special_token_re = fr"({special_token_re})".encode("utf-8")
+    special_token_re = fr"(<|endoftext|>)"# .encode("utf-8")
+    # print(special_token_re)
 
-    assert isinstance(special_token_re, bytes)
+    # assert isinstance(special_token_re, bytes)
 
-    text = text.encode("utf-8")
-    assert isinstance(text, bytes)
+    # text = text.encode("utf-8")
+    # assert isinstance(text, bytes)
 
-    PAT = r"""(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+""" 
+    # PAT = r"""(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+""" 
+    PAT = re.compile(r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+""")
 
     text_start = 0 
     text_end = -1 
     for match in re.finditer(special_token_re, text): 
         paragraph = match.group(0) 
-        paragraph = paragraph.strip()
         
         # Update the end with the start of the special token
         # and after appending to paragraph update the start with the end 
         # of the special token
-        print(match.start())
-        print(match.end())
         text_end = match.start() 
         paragraphs.append(text[text_start:text_end])
         text_start = match.end()
 
+
+    # Found myself intrigued by the fact that it 
+    # required text to be a string, and couldn't compare with 
+    # bytes requires a little more research then...     
+    paragraphs.append(text[text_start:])
+
     for paragraph in paragraphs:
-        for match in re.finditer(PAT.encode("utf-8"), paragraph):
+        for match in re.finditer(PAT, paragraph):
             word = match.group()
             # print(f"Match: {word}")
-            word = word.strip()
-            word = word.decode()
+            # word = word.decode()
+            word = word.encode("utf-8")
 
-            assert isinstance(word, str)
+            # assert isinstance(word, str)
 
-            word = ",".join(word)
-            word = word.encode("utf-8") 
+            # word = ",".join(word)
+            word = b",".join([bytes([b]) for b in word]) 
+
             # print(f"Word Type: {type(word)}")
 
             # self.vocab[word] = self.vocab_count
@@ -223,17 +229,27 @@ def callback(arg):
 # Creating function to make it easier to test
 def run_train_bpe(input_path: str, vocab_size: int, special_tokens: list[str]): 
     vocab = {}
-    freq_table = {}
+    vocab[b"<|endoftext|>"] = 0
+    vocab_count = 1
+
+    for i in range(256): 
+        letter = chr(i)
+
+        # Change this 
+        vocab[bytes([i])] = vocab_count 
+        vocab_count += 1
+
+
+    freq_table = Counter()
     file = open(input_path, "rb")
     num_process = 6 
-    vocab_count = 0
 
     # special_tokens_byte = [word.decode("utf-8") for word in special_tokens]
 
-    boundaries = find_chunk_boundaries(file, num_process, special_tokens[0])
+    boundaries = find_chunk_boundaries(file, num_process, special_tokens[0].encode("utf-8"))
     running_process = []
 
-    with Pool(processes=4) as pool:
+    with Pool(processes=6) as pool:
         for start, end in zip(boundaries[:-1], boundaries[1:]): 
             file.seek(start)
             chunk = file.read(end - start).decode("utf-8", errors="ignore")
@@ -243,91 +259,74 @@ def run_train_bpe(input_path: str, vocab_size: int, special_tokens: list[str]):
             running_process.append(process)
 
         for i in range(len(running_process)): 
-            freq_table = Counter(vocab) + Counter(running_process[i].get())
+            freq_table = Counter(freq_table) + Counter(running_process[i].get())
         # vocab = reduce(lambda a, b: Counter(a.get()) + Counter(b), running_process)
     
     merges = []
 
-    print("Pre-Tokenization")
-    print(len(freq_table))
+    vocab = dict(map(reversed, vocab.items()))
+
+    # print("Pre-Tokenization")
+    # print(len(freq_table))
 
     # Tokenization - Begin BPE process
     pairs = defaultdict(int) 
     while len(vocab) < vocab_size:
 
         for (word, count) in freq_table.items(): 
-            if word == b"<|endoftext|>" or word == b"" or len(word) < 1:
-                continue
 
-            print(f"Vocab: {word}")
-            print(f"Vocab type: {type(word)}")
-            letters = word.split(b",")
+
+            # Little hack for ",,"
+            letters = b""
+            if b",," in word: 
+                letters = [b" ", b","]
+            else: 
+                letters = [l for l in word.split(b",") if l != b""]
+            
             for i in range(1, len(letters)): 
-                pair = letters[i - 1: i + 1]
-                pair = b",".join(pair)
+                pair = (letters[i - 1], letters[i])
+                pairs[pair] += count
 
-                print(f"Pair: {pair}")
-                # Double check this area
-                pairs[pair] = count
+        pair_combined = max(pairs, key=lambda k: (pairs[k], k))  
+        # print(f"Pair combined: {pair_combined}")
 
-        pair_combined = max(pairs, key=pairs.get)  
-        print(f"Pair combined: {pair_combined}")
-
-        new_key = pair_combined.replace(b",", b"") 
-        old_key = pair_combined
-
-        pair = (pair_combined.split(b",")[0], pair_combined.split(b",")[1])
+        prefix, suffix = pair_combined
+        old_key = prefix + b"," + suffix 
+        new_key = prefix + suffix
+        pair = (prefix, suffix)
         merges.append(pair)
 
-        print(f"old_key: {old_key}")
-        print(f"new_key: {new_key}")
-
-        print(f"old key type: {type(old_key)}")
-
-        new_words = {}
-        for (word, count) in freq_table.items():   
-            print(f"Old Key: {old_key}")
-            print(f"vocab bytes: {word}")
-            print(f"count: {count}")
-
-            if old_key in word:      
-                new_word = word.replace(old_key, new_key)
-                print(f"replaced:  {new_word}")
-                new_words[new_word] = word 
-
-                # Update the freq table as well
-
-
+        new_token = prefix + suffix
+        vocab[vocab_count] = new_token
         vocab_count += 1
-        vocab[new_key] = vocab_count 
-        # self.vocab[new_key] = self.vocab_count
 
-        print("New Words")
-        print(new_words)
+        new_freq_table = defaultdict(int)
+        for (word, count) in freq_table.items():   
+            
+            if old_key in word:      
+                pattern = rb"(?<![^,])" + re.escape(old_key) + rb"(?![^,])"
+                new_word = re.sub(pattern, new_key, word)
+                new_freq_table[new_word] += count
+            else: 
+                new_freq_table[word] += count
 
-        for (new_word, old_word) in new_words.items():
-            print(f"New Vocab count: {new_word}")
-            count = freq_table[old_word]
-            freq_table[new_word] = count 
-
-            del freq_table[old_word]
-
+        freq_table = new_freq_table
         pairs.clear() 
-
-
-        print("Vocab: ")
-        print(vocab)
-
-        print("Merges: ")
-        print(merges)
         
+    # print(f"Merges: {merges}")
+    return (vocab, merges)
 
 
 if __name__ == '__main__': 
     # bpe = Tokenizer("test", 10, ["<|endoftext|>"]) 
-    encodings = run_train_bpe(
-        input_path="test_2.text",
-        vocab_size=100,
-        special_tokens=[b"<|endoftext|>"]
+    FIXTURES_PATH = (pathlib.Path(__file__).resolve().parent) / "tests" / "fixtures"
+
+    input_path = FIXTURES_PATH / "corpus.en"
+    (vocab, merges) = run_train_bpe(
+        input_path=input_path,
+        vocab_size=500,
+        special_tokens=["<|endoftext|>"]
     )
-    print(f"Encodings: {encodings}")
+
+    print(f"Vocab: {vocab}")
+    print(f"Merges: {merges}")
